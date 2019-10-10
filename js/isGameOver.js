238362f543ce75660f4e173986fa8d08dec49d6a
Joshua <joshua.cook@onthebeach.co.uk>
2019-10-10 14:11:24 +0100

    Refactor all files with prettier, change variable names and update CSS

--- js/isGameOver.js
+++ js/isGameOver.js
@@ -1,25 +1,26 @@
 //Using set theory to set solutions
 const solutions = [
-    ['A', 'B', 'C'],
-    ['A', 'E', 'I'],
-    ['A', 'D', 'G'],
-    ['B', 'E', 'H'],
-    ['C', 'E', 'G'],
-    ['C', 'F', 'I'],
-    ['D', 'E', 'F'],
-    ['G', 'H', 'I'],
+  ["A", "B", "C"],
+  ["A", "E", "I"],
+  ["A", "D", "G"],
+  ["B", "E", "H"],
+  ["C", "E", "G"],
+  ["C", "F", "I"],
+  ["D", "E", "F"],
+  ["G", "H", "I"]
 ];
 
 function isGameOver(playerAnswer) {
   let gameOver = false;
 
   solutions.forEach(solution => {
-    gameOver = gameOver || solution.every(letter => playerAnswer.includes(letter));
+    gameOver =
+      gameOver || solution.every(letter => playerAnswer.includes(letter));
   });
 
   return gameOver;
 }
 
 if (module) {
-  module.exports = {isGameOver: isGameOver, solutions: solutions};
-};
+  module.exports = { isGameOver: isGameOver, solutions: solutions };
+}

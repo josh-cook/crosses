7c7565c9f96e635fad0ad125e18422892a5e87af
joshua-cook <joshua.cook@onthebeach.co.uk>
2020-01-24 13:49:21 +0000

    Add better styling and functionality works properly

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
-const playerAnswer = [];
+let playerAnswer = [];
 
 //removing colours from nodes and clearing answer array back to start game state
-function reset() {
+const reset = _ => {
   playerAnswer = [];
   $(".cross-node").removeClass("colour");
 }
 
 //needs better way of creating a new game
-$(".cross-node").on("click", function(e) {
+$(".cross-node").on("click", (e) => {
   playerAnswer.push(e.target.id);
   $(e.target).addClass("colour");
   if (isGameOver(playerAnswer)) {
